5ca711b338794153ea17ed295b61c801392cd2e2
Tom Stuart <tom@codon.com>
2017-03-04 09:54:29 +0000

    Validate presence and uniqueness of event name and year
    It seems sensible that the event name should be unique for that
    particular year, i.e. the overall string representation of the event
    (“RubyConf 2017”) identifies it uniquely.
    
    It may not be true that all events necessarily have a year in their
    name, but the app already makes that assumption so we may as well codify
    it in the validations. (If the assumption changes, the validations will
    have to change too.)

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :year
+  validates_uniqueness_of :name, scope: :year
+
   def name_and_year
     "#{name} #{year}"
   end
