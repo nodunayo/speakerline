785a6ab7ba93b07c2ad77ac3f8b5fe146b3719e8
John Magee <jemagee@cox.net>
2017-03-06 19:33:09 -0800

    Changing Year to Integer in Events while providing code to preserve current records

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   validates_presence_of :name
-  validates_presence_of :year
+  validates :year, presence: true, numericality: {only_integer: true}
   validates_uniqueness_of :name, scope: :year
 
   def name_and_year
