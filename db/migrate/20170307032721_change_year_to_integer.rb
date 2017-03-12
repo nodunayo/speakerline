22af498f9d3199d94437f7c1f976fba432aeb16e
John Magee <jemagee@cox.net>
2017-03-11 16:14:18 -0800

    Altering the migration file to hopefully decouple things properly

--- db/migrate/20170307032721_change_year_to_integer.rb
+++ db/migrate/20170307032721_change_year_to_integer.rb
@@ -2,26 +2,25 @@ class ChangeYearToInteger < ActiveRecord::Migration[5.0]
 
 
   def up
-  	events = Event.pluck(:id, :year)
-  	remove_column :events, :year
-  	add_column :events, :year, :integer, null: false
 
-  	events.each do |event|
-  		id = event[0]
-  		year = event[1].to_i
-  		Event.find(id).update_attributes!(year: year)
-  	end
+  	add_column :events, :year_integer, :integer
+    execute <<-SQL
+      UPDATE events
+      SET year_integer = CAST(year as integer);
+    SQL
+  	remove_column :events, :year
+    rename_column :events, :year_integer, :year
+    change_column :events, :year, :integer, null: false
   end
 
   def down
-  	events = Event.pluck(:id, :year)
-  	remove_column :events, :year
-  	add_column :event, :year, :string, null: false
-
-  	events.each do |event|
-  		id = event[0]
-  		year = event[1].to_s
-  		Event.find(id).update_attributes!(year: year)
-  	end
+  	add_column :events, :year_string, :string
+    execute <<-SQL
+      UPDATE events
+      SET year_string = CAST(year as varchar);
+    SQL
+    remove_column :events, :year
+    rename_column :events, :year_string, :year
+    change_column :events, :year, :string, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307032721) do
+ActiveRecord::Schema.define(version: 20170305210109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20170307032721) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "year",       null: false
+    t.string   "year",       null: false
   end
 
   create_table "proposals", force: :cascade do |t|
