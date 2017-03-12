582c3532d33d35c1bf8ac1f0591af3de574e3b10
John Magee <jemagee@cox.net>
2017-03-11 17:33:43 -0800

    Basic Proposal Editing and BDD testing

---
+++ features/step_definitions/editing_proposals.rb
@@ -0,0 +1,20 @@
+When(/^I click 'Edit Proposal'/) do
+	page.click_on('Edit proposal')
+end
+
+When(/^I change the title to 'Reading Code Well'/) do
+	page.fill_in(:proposal_title, with: 'Reading Code Well')
+end
+
+When(/^I click 'Update Proposal'/) do
+	page.click_on('Update proposal')
+end
+
+Then(/I should see 'Reading Code Well'/) do
+	expect(page).to have_content("Reading Code Well")
+end
+
+Then(/I should not see 'Reading Code Good'/) do
+	expect(page).to_not have_content("Reading Code Good")
+end
+
