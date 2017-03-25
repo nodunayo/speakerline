d3c52f844eb68aa4ec6767ee64ac6ab97593b51f
John Magee <jemagee@cox.net>
2017-03-25 12:07:16 -0700

    Update editing_proposals.rb
    Changing indent from 4 to 2

--- features/step_definitions/editing_proposals.rb
+++ features/step_definitions/editing_proposals.rb
@@ -1,20 +1,20 @@
 When(/^I click 'Edit Proposal'/) do
-	page.click_on('Edit proposal')
+  page.click_on('Edit proposal')
 end
 
 When(/^I change the title to 'Reading Code Well'/) do
-	page.fill_in(:proposal_title, with: 'Reading Code Well')
+  page.fill_in(:proposal_title, with: 'Reading Code Well')
 end
 
 When(/^I click 'Update Proposal'/) do
-	page.click_on('Update proposal')
+  page.click_on('Update proposal')
 end
 
 Then(/I should see 'Reading Code Well'/) do
-	expect(page).to have_content("Reading Code Well")
+  expect(page).to have_content("Reading Code Well")
 end
 
 Then(/I should not see 'Reading Code Good'/) do
-	expect(page).to_not have_content("Reading Code Good")
+  expect(page).to_not have_content("Reading Code Good")
 end
 
