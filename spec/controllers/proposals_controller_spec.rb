7e4cfc1488da19eea2c2e2084f8eeaeef4f59978
Andrew Nesbitt <andrewnez@gmail.com>
2017-05-29 17:47:10 +0100

    Fixed proposal spec

--- spec/controllers/proposals_controller_spec.rb
+++ spec/controllers/proposals_controller_spec.rb
@@ -7,21 +7,25 @@ RSpec.describe ProposalsController do
       let(:expected_proposal) { Proposal.new(id: 7, title: 'A great talk', body: 'Come listen to a great talk', speaker_id: 5 ) }
 
       before do
-        allow(Proposal).to receive(:create!).and_return(expected_proposal)
+        allow(Proposal).to receive(:new).and_return(expected_proposal)
+        allow(expected_proposal).to receive(:save).and_return(true)
         post :create, params: { proposal: { title: 'A great talk', body: 'Come listen to a great talk', speaker_id: 5 } }
       end
- 
+
       it 'creates a new proposal' do
-        expect(Proposal).to have_received(:create!).with(title: 'A great talk', body: 'Come listen to a great talk', speaker_id: '5')
+        expect(Proposal).to have_received(:new).with(title: 'A great talk', body: 'Come listen to a great talk', speaker_id: '5')
       end
-      
-      it { should redirect_to(proposal_path(assigns(:proposal))) }
+
+      it { should redirect_to(proposal_path(expected_proposal)) }
     end
 
     context 'with invalid attributes' do
+      let(:invalid_proposal) { Proposal.new(title: nil) }
+
       before do
-        allow(Proposal).to receive(:create!) { fail ActiveRecord::ActiveRecordError }
-        post :create, params: { proposal: { name: '' } }
+        allow(Proposal).to receive(:new).and_return(invalid_proposal)
+        allow(invalid_proposal).to receive(:save).and_return(false)
+        post :create, params: { proposal: { title: '' } }
       end
 
       it { is_expected.to set_flash[:alert].to('Failed to save proposal') }
@@ -29,5 +33,3 @@ RSpec.describe ProposalsController do
     end
   end
 end
-
-
