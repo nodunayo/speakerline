f0e44e0a73ae2bbb0549b8b235da615c3d8ec7db
Nadia Odunayo <nodunayo@gmail.com>
2019-02-23 17:06:25 +0000

    Fix shoulda_matcher deprecation warning

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -4,5 +4,5 @@ RSpec.describe Submission do
   it { should validate_presence_of(:event_instance) }
   it { should validate_presence_of(:proposal) }
   it { should validate_presence_of(:result) }
-  it { should define_enum_for(:result).with([:accepted, :rejected, :waitlisted]) }
+  it { should define_enum_for(:result).with_values([:accepted, :rejected, :waitlisted]) }
 end
