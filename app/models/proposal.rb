0e228e85e04a24c13c0dd3efc82faf93c548ff9d
Nadia Odunayo <nodunayo@gmail.com>
2019-03-18 10:02:58 +0000

    Proposals can only have a maximum of 3 tags

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -5,5 +5,12 @@ class Proposal < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :body
 
+
   acts_as_taggable
+  validate :maximum_amount_of_tags
+
+  def maximum_amount_of_tags
+    number_of_tags = tag_list_cache_on("tags").uniq.length
+    errors.add(:base, "must only have a maximum of 3 tags") if number_of_tags > 3
+  end
 end
--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -3,4 +3,11 @@ require 'rails_helper'
 RSpec.describe Proposal do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
+
+  it 'can only have up to 3 tags' do
+    proposal = create(:proposal)
+    proposal.tag_list.add("this", "is", "a", "great", "proposal")
+    proposal.valid?
+    expect(proposal.errors[:base]).to include("must only have a maximum of 3 tags")
+  end
 end
