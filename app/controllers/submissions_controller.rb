278d83e4b6772eb8dc3765c0139feb31ddd7c1f7
Nadia Odunayo <nodunayo@gmail.com>
2020-04-12 07:44:07 +0100

    A user has to be signed in to add or edit a submission

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,11 +1,17 @@
 class SubmissionsController < ApplicationController
   def new
-    @proposal = Proposal.find(params[:proposal])
-    @submission = Submission.new(proposal_id: @proposal.id)
-    @events = Event.all.order('name ASC')
+    if session[:current_user_id]
+      @proposal = Proposal.find(params[:proposal])
+      @submission = Submission.new(proposal_id: @proposal.id)
+      @events = Event.all.order('name ASC')
+    else
+      redirect_to "#{ENV["DID_AUTHENTICATION_ENDPOINT"]}?client_id=#{ENV["DID_CLIENT_ID"] || "test_0xKvM6N9"}&redirect_uri=#{session_callback_url}"
+    end
   end
 
   def create
+    return if !session[:current_user_id]
+
     @submission = Submission.new(submission_params)
     if verify_recaptcha(model: @submission) && @submission.save
       redirect_to proposal_path(@submission.proposal)
@@ -17,11 +23,20 @@ class SubmissionsController < ApplicationController
   end
 
   def edit
-    @submission = Submission.find(params[:id])
-    @proposal = @submission.proposal
+    if session[:current_user_id]
+      @submission = Submission.find(params[:id])
+      @proposal = @submission.proposal
+    else
+      redirect_to "#{ENV["DID_AUTHENTICATION_ENDPOINT"]}?client_id=#{ENV["DID_CLIENT_ID"] || "test_0xKvM6N9"}&redirect_uri=#{session_callback_url}"
+    end
   end
 
   def update
+    if !session[:current_user_id]
+      redirect_to "#{ENV["DID_AUTHENTICATION_ENDPOINT"]}?client_id=#{ENV["DID_CLIENT_ID"] || "test_0xKvM6N9"}&redirect_uri=#{session_callback_url}"
+      return
+    end
+
     @submission = Submission.find(params[:id])
     @proposal = @submission.proposal
     if verify_recaptcha(model: @submission) && @submission.update(submission_params)
