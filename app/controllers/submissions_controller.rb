8dd254baa484b609bca64653f654cb39710e5e8f
Hilary Stohs-Krause <hilarysk@gmail.com>
2024-05-08 16:27:10 -0400

    Issue #579 - If I accidentally select the wrong conference for a submission, I'd like to be able to change it.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -17,6 +17,7 @@ class SubmissionsController < ApplicationController
   end
 
   def edit
+    @events = Event.all.order('name ASC')
     @submission = Submission.find(params[:id])
     @proposal = @submission.proposal
   end
